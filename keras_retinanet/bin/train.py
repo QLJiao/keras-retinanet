e07754d644cced572c76fc782d7bf503537f6a5b
JqL <897869389@qq.com>
2018-10-09 16:07:39 +0800

    3rd train

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -231,7 +231,7 @@ def create_generators(args, preprocess_image):
             min_shear=-0.1,
             max_shear=0.1,
             min_scaling=(0.8, 0.8),
-            max_scaling=(1.1, 1.1),
+            max_scaling=(1.2, 1.2),
             flip_x_chance=0.5,
             flip_y_chance=0.5,
         )
@@ -397,7 +397,7 @@ def parse_args(args):
     parser.add_argument('--multi-gpu',        help='Number of GPUs to use for parallel processing.', type=int, default=0)
     parser.add_argument('--multi-gpu-force',  help='Extra flag needed to enable (experimental) multi-gpu support.', action='store_true')
     parser.add_argument('--epochs',           help='Number of epochs to train.', type=int, default=1)
-    parser.add_argument('--steps',            help='Number of steps per epoch.', type=int, default=18000)
+    parser.add_argument('--steps',            help='Number of steps per epoch.', type=int, default=17000)
     parser.add_argument('--snapshot-path',    help='Path to store snapshots of models during training (defaults to \'./snapshots\')', default='./snapshots')
     parser.add_argument('--tensorboard-dir',  help='Log directory for Tensorboard output', default='./logs')
     parser.add_argument('--no-snapshots',     help='Disable saving snapshots.', dest='snapshots', action='store_false')
--- keras_retinanet/bin/train.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python train.py --gpu=1 --backbone=resnet152 --random-transform pascal /home/ustc/jql/VOCdevkit2007/VOC2007
